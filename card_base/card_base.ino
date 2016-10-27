28f18ee1422fdee497cbd8efc1d11362a308eef6
Jeff Pflueger <jeffrey.pflueger@students.olin.edu>
2016-10-27 14:29:26 -0400

    Added untangle function

--- card_base/card_base.ino
+++ card_base/card_base.ino
@@ -6,29 +6,58 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 
 // Stepper Motor with 1.8 degrees per step
 Adafruit_StepperMotor *rotational = AFMS.getStepper(200, 1);
-
+Adafruit_DCMotor *cardspitter = AFMS.getMotor(3);
+int loops = 0;
 int cardSpitter = 9;
-
+int steps = 40;
+int stepCount = 0;
+bool end = false;
 void setup() {
   Serial.begin(9600); 
   AFMS.begin();
   
   rotational->setSpeed(10);
-  pinMode(cardSpitter, OUTPUT);
+  cardspitter->setSpeed(100);
 }
 
 void loop() {
-  rotateMotor72degrees();
-  spitCards();
+  rotateMotor72degrees((!end));
+  spitCard();
   delay(500);
+  loops++;
+  if (loops > 4){
+    end = true;
+  }
+  if(end){
+    untangle();
+    rotational->setSpeed(0);
+    cardspitter->setSpeed(0);
+    stepCount = 0;
+  }
+  
 }
 
-void rotateMotor72degrees() {
-  rotational->step(40, FORWARD, DOUBLE);
+void rotateMotor72degrees(bool go) {
+  if(go){
+  rotational->step(steps, FORWARD, DOUBLE);
+  stepCount += steps;
+  }
+}
+void untangle(){
+  rotational->step(stepCount, BACKWARD, DOUBLE);
+  
+}
+void spitCards(int n){
+  for(int i = 0; i <n; i++){
+    spitCard();
+    delay(100);
+  }
 }
 
-void spitCards() {
-  analogWrite(cardSpitter, 250);
-  delay(100);
-  analogWrite(cardSpitter, 0); 
+void spitCard() {
+
+  cardspitter->run(FORWARD);
+  delay(75);
+  cardspitter->run(RELEASE);
+ 
 }
